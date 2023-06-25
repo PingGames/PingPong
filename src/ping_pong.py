cdbbd5e7a38155cdfa01d5c7cf3ce050a787fed1
PingGames <94293156+PingGames@users.noreply.github.com>
2023-06-25 17:09:25 +0200

    Draw ball sprite to game window

--- src/ping_pong.py
+++ src/ping_pong.py
@@ -1,8 +1,32 @@
 import pygame
+import os
 
 pygame.init()
 screen = pygame.display.set_mode((640, 480))
 pygame.display.set_caption("PingPong")
+
+image_folder = "extra/images"
+image_path = os.path.join(image_folder, "ball.png")
+ball_image = pygame.image.load(image_path).convert_alpha()
+
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = ball_image
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def update(self):
+        pass
+
+
+all_sprites = pygame.sprite.Group()
+
+ball = Ball(295, 215)
+all_sprites.add(ball)
+
 
 game_is_active = True
 
@@ -13,6 +37,11 @@ while game_is_active:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_is_active = False
+
+    screen.fill((255, 255, 255))
+
+    all_sprites.update()
+    all_sprites.draw(screen)
 
     pygame.display.flip()
 
